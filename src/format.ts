7d081928cd64e3a850d00e5d87b0824e298e57c1
nixenoodle <github.bogged110@passmail.net>
2026-07-12 22:49:31 +0100

    Add support for .ogg audio files

--- src/format.ts
+++ src/format.ts
@@ -16,7 +16,7 @@ const DISPLAY_CODE_REPLACE: string = "OBSTOANKICODEDISPLAY"
 const CLOZE_REGEXP: RegExp = /(?:(?<!{){(?:c?(\d+)[:|])?(?!{))((?:[^\n][\n]?)+?)(?:(?<!})}(?!}))/g
 
 const IMAGE_EXTS: string[] = [".png", ".jpg", ".jpeg", ".gif", ".bmp", ".svg", ".tiff"]
-const AUDIO_EXTS: string[] = [".wav", ".m4a", ".flac", ".mp3", ".wma", ".aac", ".webm"]
+const AUDIO_EXTS: string[] = [".wav", ".m4a", ".flac", ".mp3", ".wma", ".aac", ".webm", ".ogg"]
 
 const PARA_OPEN: string = "<p>"
 const PARA_CLOSE: string = "</p>"
